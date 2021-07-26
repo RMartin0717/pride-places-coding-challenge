6040c0eb56f6f0e871485994eceb28ac7d8be467
Riley Martin <riley.willow@gmail.com>
2021-07-26 11:19:29 -0600

    Build out general class component structure for AllPosts

--- src/components/AllPosts/AllPosts.js
+++ src/components/AllPosts/AllPosts.js
@@ -1,1 +1,21 @@
+import React, { Component } from 'react'
+import './AllPosts.css'
 
+class AllPosts extents Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h3>All Posts Previews</h3>
+      </>
+    )
+  }
+}
+
+export default AllPosts
