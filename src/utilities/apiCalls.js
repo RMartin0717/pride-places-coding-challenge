993163c5ec46c9a828ee6e5e959c70bc9831d026
Riley Martin <riley.willow@gmail.com>
2021-07-26 11:51:40 -0600

    Write GET request for posts data

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,9 +1,16 @@
 const retrievePostsData = () => {
-
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('No posts available. Please try again later.')
+      } else {
+        return response.json()
+      }
+    })
 }
 
 const retrieveUserData = (id) => {
-
+  //use interpolation later to get user data by id
 }
 
-export { retrievePostsData, retrieveUserData }
+export { retrievePostsData }
