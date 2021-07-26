9753ff372fd3fb9e4f19fe5365a2b01cfc6b7614
Riley Martin <riley.willow@gmail.com>
2021-07-26 13:52:11 -0600

    Write GET request for specific user data

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,7 +1,7 @@
 const retrievePostsData = () => {
   return fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => {
-      if(!response.ok) {
+      if (!response.ok) {
         throw new Error('No posts available. Please try again later.')
       } else {
         return response.json()
@@ -9,8 +9,15 @@ const retrievePostsData = () => {
     })
 }
 
-// const retrieveUserData = (id) => {
-//   //use interpolation later to get user data by id
-// }
+const retrieveUserData = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/users?id=${id}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Could not find user information.')
+      } else {
+        return response.json()
+      }
+    })
+}
 
-export { retrievePostsData }
+export { retrievePostsData, retrieveUserData }
