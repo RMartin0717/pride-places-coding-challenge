466af848d5b503718330805e3d3563d8734820a3
Riley Martin <riley.willow@gmail.com>
2021-07-26 11:18:47 -0600

    Set up components file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Posts</h1>
+
+    </main>
+  )
+}
+
+export default App;
---
+++ src/components/PostPreview/PostPreview.js
@@ -0,0 +1,3 @@
+import './PostPreview.css'
+
+const
